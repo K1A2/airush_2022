b9484547d93d7f5a553b49f6db1b4eddea42dced
K1A2 <jckim0307@gmail.com>
2022-07-14 00:01:22 +0900

    as

--- auto_submit.py
+++ auto_submit.py
@@ -7,17 +7,20 @@ def run(cmd):
     return completed
 
 if __name__ == '__main__':
-    for i in range(8, 41):
-        is_succ = 0
-        command = f"nsml submit KR96310/airush2022-1-2a/79 {i} --esm 96310"
-        while not is_succ:
-            print('execute commands:', command)
-            info = run(command)
-            if info.returncode != 0:
-                print("An error occured\n%s", info.stderr.decode("utf-8"))
-            else:
-                print("executed successfully!")
-                print(info.stdout.decode("utf-8"))
-                is_succ = 1
-            if not is_succ:
-                time.sleep(60 * 5)
+    n = [202]
+    l = [[0, 20]]
+    for j in range(2):
+        for i in range(l[j][0], l[j][1]):
+            is_succ = 0
+            command = f"nsml submit KR96310/airush2022-1-2a/{n[j]} {i} --esm 96310"
+            while not is_succ:
+                print('execute commands:', command)
+                info = run(command)
+                if info.returncode != 0:
+                    print("An error occured\n%s", info.stderr.decode("utf-8"))
+                else:
+                    print("executed successfully!")
+                    print(info.stdout.decode("utf-8"))
+                    is_succ = 1
+                if not is_succ:
+                    time.sleep(60 * 5)
